a3c5760bf83f260108f123dab0a7fa4f7e3d96e8
Cameron Brown <veiledjune@gmail.com>
2026-02-25 17:40:34 +0200

    feat(events): Add playButtonEvents function and update events logic to use game state tracking

--- src/scripts/events.js
+++ src/scripts/events.js
@@ -1,15 +1,21 @@
 import { render } from './render.js';
 import { getCoordinates } from './getCoordinates.js';
 import { validateShipPlacement } from './validate.js';
+import { playGame } from './app.js';
 
 export const events = (() => {
+  const gameState = playGame.getGameState();
+
   const moveShipEvent = (player, ship, index) => {
+    if (gameState.gameActive) return;
     render.renderSelectShip(ship);
     const rerender = player.game.moveShip(ship, index);
     if (rerender) render.renderBoard(player);
   };
 
   const shipHoverEvent = (player, newIndex) => {
+    if (gameState.gameActive) return;
+
     const shipState = player.game.getShipState();
     if (!shipState.selectedShip) return;
     const newCoor = getCoordinates.getNewCoordinates(
@@ -32,5 +38,21 @@ export const events = (() => {
     );
     render.renderHover(isValid, newCoor);
   };
-  return { moveShipEvent, shipHoverEvent };
+
+  const playButtonEvents = () => {
+    const playButton = document.querySelector('.play-btn');
+    console.log(playButton);
+
+    if (!gameState.gameActive) {
+      playButton.textContent = 'Quit';
+      playButton.classList.remove('--btn-active');
+      console.log(gameState);
+    } else {
+      playButton.textContent = 'Start';
+      playButton.classList.add('--btn-active');
+    }
+    gameState.gameActive = !gameState.gameActive;
+    console.log(gameState);
+  };
+  return { moveShipEvent, shipHoverEvent, playButtonEvents };
 })();
