808e63ef416c986dd3819e1a513ed2b39435d559
Cameron Brown <veiledjune@gmail.com>
2026-02-26 20:21:11 +0200

    feat(audio): Add playMiss and playHit function
    Add functions to handle playing game sound effects

--- src/scripts/audio.js
+++ src/scripts/audio.js
@@ -3,3 +3,17 @@ export function playSong() {
   audio.loop = true;
   audio.play();
 }
+
+export const gameSounds = (() => {
+  const miss = new Audio('./audio/miss.mp3');
+  const hit = new Audio('./audio/hit.mp3');
+  const playMiss = () => {
+    miss.currentTime = 0;
+    miss.play();
+  };
+  const playHit = () => {
+    hit.currentTime = 0;
+    hit.play();
+  };
+  return { playMiss, playHit };
+})();
